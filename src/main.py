bc2bddff5e356c7b9691a1bf904d9ac2ce8dba66
Ratindra Parate <22uec108@lnmiit.ac.in>
2025-12-17 06:31:00 +0530

    Add command-line interaction to student management system

--- src/main.py
+++ src/main.py
@@ -2,9 +2,29 @@ from utils import add_student, get_students
 
 students = []
 
-print(add_student(students, "Rahul"))
-print(add_student(students, "Anita"))
+while True:
+    print("\nStudent Management System")
+    print("1. Add student")
+    print("2. View students")
+    print("3. Exit")
 
-print("\nStudent List:")
-for student in get_students(students):
-    print("-", student)
+    choice = input("Enter your choice (1/2/3): ")
+
+    if choice == "1":
+        name = input("Enter student name: ")
+        print(add_student(students, name))
+
+    elif choice == "2":
+        print("\nStudent List:")
+        if not students:
+            print("No students added yet.")
+        else:
+            for s in get_students(students):
+                print("-", s)
+
+    elif choice == "3":
+        print("Exiting program. Goodbye!")
+        break
+
+    else:
+        print("Invalid choice. Please try again.")
